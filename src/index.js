e242cd447d37519fe845dc54b36d51b3e1f4a2a2
Sean McNally <seanmcnally88@gmail.com>
2017-07-25 07:56:22 -0500

    yt search in constructor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBRrKYyGMooeG_nFxKrzGgjnsV1jNsZ2WE';
-// Create a new component. This component should produce html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
+      this.setState({
+        videos
+      })
+    });
+  }
+
+  render() {
+      return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  };
 }
-// take this component's generated html and put in on the page/in the DOM
+
 ReactDOM.render(<App />, document.querySelector('.container'));
